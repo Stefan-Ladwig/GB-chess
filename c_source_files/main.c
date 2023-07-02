1b20acb5c465d29db33c489ca1dec5bbefc1e84d
Stefan Ladwig <ladwig.stefan@gmx.de>
2023-07-02 19:44:47 +0200

    compilable with tiles and map for background

--- c_source_files/main.c
+++ c_source_files/main.c
@@ -1,5 +1,20 @@
 #include <gb/gb.h>
+#include <stdint.h>
+#include <gb/hardware.h>
+#include "../resources/chess_tilemap.h"
+#include "../resources/chess_tiles.h"
 
 
 int main(void)
-{}
+{
+    disable_interrupts();
+    DISPLAY_OFF;
+    
+    set_bkg_data(0, 99, chess_tiles);
+    set_bkg_tiles(0, 0, 20, 18, chess_tilemap);
+    SHOW_BKG;
+
+    DISPLAY_ON;
+    enable_interrupts();
+    return 0;
+}
