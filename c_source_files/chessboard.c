d96eb688325dae0afcd3a16884d794c29de8a222
Stefan Ladwig <ladwig.stefan@gmx.de>
2023-07-04 01:20:35 +0200

    func to get legal moves done, needs more testing

--- c_source_files/chessboard.c
+++ c_source_files/chessboard.c
@@ -48,6 +48,13 @@ void move_piece(const uint8_t origin[2], const uint8_t destination[2])
 }
 
 
+bool is_possible_destination(uint8_t row, uint8_t col, uint8_t new_row, uint8_t new_col)
+{
+    return (chessboard[new_row][new_col] == no_Piece ||
+            get_color(chessboard[new_row][new_col]) != get_color(chessboard[row][col]));
+}
+
+
 void add_possible_destination(uint8_t ***possible_destinations, uint8_t new_row,
                               uint8_t new_col, uint8_t *num_solutions)
 {
@@ -59,6 +66,31 @@ void add_possible_destination(uint8_t ***possible_destinations, uint8_t new_row,
     new_destination[1] = new_col;
     (*possible_destinations)[(*num_solutions) - 1] = new_destination;
     (*possible_destinations)[(*num_solutions)] = (uint8_t*) NULL;
+}
+
+
+void get_destinations_from_move_set(uint8_t row, uint8_t col, uint8_t *num_solutions, uint8_t num_moves,
+                                    uint8_t move_set[][2], bool repeatable, uint8_t ***possible_destinations)
+{
+    uint8_t new_row = 0;
+    uint8_t new_col = 0;
+
+    for (uint8_t i = 0; i < num_moves; i++)
+    {
+        for (uint8_t j = 0; j < 8; j++)
+        {
+            new_row = row + (j + 1) * move_set[i][0];
+            new_col = col + (j + 1) * move_set[i][1];
+            if (index_out_of_range(new_row) || index_out_of_range(new_col))
+                continue;
+            
+            if (is_possible_destination(row, col, new_row, new_col))
+                add_possible_destination(possible_destinations, new_row, new_col, num_solutions);
+            
+            if ((!repeatable) || (repeatable && chessboard[new_row][new_col] != no_Piece))
+                break;
+        }
+    }
 }
 
 
@@ -115,23 +147,46 @@ uint8_t **get_possible_destinations(const uint8_t origin[2])
         break;
 
     case Knight:
-        
+        ;
+        const int8_t knight_moves[8][2] = { {1, 2}, {2, 1}, {-1, 2}, {-2, 1},
+                                            {1, -2}, {2, -1}, {-1, -2}, {-2, -1}};
+
+        get_destinations_from_move_set(row, col, &num_solutions, 8, knight_moves, false, &possible_destinations);
+
         break;
 
     case Bishop:
-        
+        ;
+        const int8_t bishop_moves[4][2] = { {1, 1}, {1, -1}, {-1, 1}, {-1, -1} };
+
+        get_destinations_from_move_set(row, col, &num_solutions, 4, bishop_moves, true, &possible_destinations);
+    
         break;
 
     case Rook:
-        
+        ;
+        const int8_t rook_moves[4][2] = { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
+
+        get_destinations_from_move_set(row, col, &num_solutions, 4, bishop_moves, true, &possible_destinations);
+    
         break;
 
     case Queen:
-        
+        ;
+        const int8_t queen_moves[8][2] = { {1, 0}, {0, 1}, {-1, 0}, {0, -1},
+                                           {1, 1}, {1, -1}, {-1, 1}, {-1, -1} };
+
+        get_destinations_from_move_set(row, col, &num_solutions, 8, bishop_moves, true, &possible_destinations);
+    
         break;
         
     case King:
-        
+        ;
+        const int8_t king_moves[8][2] = { {1, 0}, {0, 1}, {-1, 0}, {0, -1},
+                                           {1, 1}, {1, -1}, {-1, 1}, {-1, -1} };
+
+        get_destinations_from_move_set(row, col, &num_solutions, 8, bishop_moves, false, &possible_destinations);
+    
         break;
     }
     return possible_destinations;
