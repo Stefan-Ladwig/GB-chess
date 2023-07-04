c80fdb58b609e63141ff28400db17072ad95be7f
Stefan Ladwig <ladwig.stefan@gmx.de>
2023-07-04 20:38:13 +0200

    moving cursor + select squares, working

--- c_source_files/input.c
+++ c_source_files/input.c
@@ -1,6 +1,7 @@
-#include <gb/hardware.h>
 #include <gb/gb.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
 #include "chessboard.h"
 #include "graphics.h"
 
@@ -8,49 +9,71 @@ const uint8_t DPAD_PRESSED = J_LEFT | J_RIGHT | J_UP | J_DOWN;
 
 uint8_t joypad_state = 0;
 
-struct cursor {
+bool square_selected = false;
+
+struct pos {
     uint8_t x;
     uint8_t y;
-} cursor;
+} cursor, selection;
 
 
 void init_cursor()
 {
+    selection.x = 0;
+    selection.y = 0;
+    
     cursor.x = 3;
     cursor.y = 6;
 
-    move_selection_sprites(cursor.x, cursor.y);
+    move_cursor_sprites(cursor.x, cursor.y);
+}
+
+
+int8_t *get_direction(uint8_t joypad_state)
+{
+    int8_t *direction = malloc(2 * sizeof(int8_t));
+
+    switch (joypad_state)
+    {
+    case J_LEFT:
+        direction[0] = -1;
+        break;
+
+    case J_RIGHT:
+        direction[0] = 1;
+        break;
+
+    case J_UP:
+        direction[1] = -1;
+        break;
+
+    case J_DOWN:
+        direction[1] = 1;
+        break;
+    }
+    return direction;
 }
 
 
 void move_cursor(uint8_t joypad_state)
 {    
-    int8_t x = 0, y = 0;
+    int8_t *dir = get_direction(joypad_state);
 
-    switch (joypad_state)
-    {
-    case J_LEFT:
-        x = -1;
-        break;
+    cursor.x = (cursor.x + dir[0] + 8) % 8;
+    cursor.y = (cursor.y + dir[1] + 8) % 8;
 
-    case J_RIGHT:
-        x = 1;
-        break;
+    free(dir);
+}
 
-    case J_UP:
-        y = -1;
-        break;
 
-    case J_DOWN:
-        y = 1;
-        break;
-    
-    default:
-        return;
-    }
+void move_selection(uint8_t joypad_state)
+{    
+    int8_t *dir = get_direction(joypad_state);
 
-    cursor.x = (cursor.x + x + 8) % 8;
-    cursor.y = (cursor.y + y + 8) % 8;
+    selection.x = (selection.x + dir[0] + 8) % 8;
+    selection.y = (selection.y + dir[1] + 8) % 8;
+
+    free(dir);
 }
 
 
@@ -61,8 +84,39 @@ void handle_input()
 
         if (joypad_state & DPAD_PRESSED)
         {
-            move_cursor(joypad_state);
-            move_selection_sprites(cursor.x, cursor.y);
+            if (square_selected)
+            {
+                move_selection(joypad_state);
+                move_selection_sprites(selection.x, selection.y);
+            }
+            else
+            {
+                move_cursor(joypad_state);
+                move_cursor_sprites(cursor.x, cursor.y);
+            }
+        }
+        else if (joypad_state & J_A)
+        {
+            if (!square_selected && piece_on_square(cursor.y, cursor.x))
+            {
+                move_selection_sprites(cursor.x, cursor.y);
+                selection.x = cursor.x;
+                selection.y = cursor.y;
+                square_selected = true;
+            }
+            else if (square_selected && move_is_legal(cursor.y, cursor.x, selection.y, selection.x))
+            {
+                move_cursor_sprites(selection.x, selection.y);
+                cursor.x = selection.x;
+                cursor.y = selection.y;
+                square_selected = false;
+                hide_selection();
+            }
+            else
+            {
+                square_selected = false;
+                hide_selection();
+            }
         }
 
         waitpadup();
