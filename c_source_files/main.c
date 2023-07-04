274781c68a378c4ab08d0663def7922ecbb4b4f5
Stefan Ladwig <ladwig.stefan@gmx.de>
2023-07-04 20:42:01 +0200

    main now only includes game.h

--- c_source_files/main.c
+++ c_source_files/main.c
@@ -1,21 +1,12 @@
-#include <gb/gb.h>
-#include <stdint.h>
-#include <gb/hardware.h>
-#include "../resources/chess_tilemap.h"
-#include "../resources/chess_tiles.h"
-#include "chessboard.h"
+#include "game.h"
 
 
-int main(void)
+void main(void)
 {
-    disable_interrupts();
-    DISPLAY_OFF;
+    init_game();
 
-    set_bkg_data(0, 99, chess_tiles);
-    set_bkg_tiles(0, 0, 20, 18, chess_tilemap);
-    SHOW_BKG;
-
-    DISPLAY_ON;
-    enable_interrupts();
-    return 0;
+    while (1)
+    {
+        handle_input();
+    }
 }
