f03b30ff7100ff04eac49e9d3f66ec1d8aee1fa1
Stefan Ladwig <ladwig.stefan@gmx.de>
2023-07-09 15:32:24 +0200

    moves are now tracked, implemented replay mode

--- c_source_files/game.c
+++ c_source_files/game.c
@@ -10,7 +10,10 @@ const uint8_t DPAD_PRESSED = J_LEFT | J_RIGHT | J_UP | J_DOWN;
 uint8_t joypad_state;
 bool square_selected;
 bool player;
+bool replay_mode = false;
 uint8_t event;
+uint8_t **list_of_moves;
+uint16_t num_moves;
 
 struct pos {
     uint8_t x;
@@ -30,16 +33,45 @@ void init_cursor()
 }
 
 
+void init_list_of_moves()
+{
+    for (uint8_t i = 0; i < num_moves; i++)
+    {
+        free(list_of_moves[i]);
+    }    
+
+    num_moves = 0;
+    list_of_moves = realloc(list_of_moves, 0);
+}
+
+
 void init_game()
 {
     init_board();
     init_graphics();
     init_cursor();
+    if (!replay_mode) init_list_of_moves();
 
     joypad_state = 0;
     square_selected = false;
-    player = 0;
+    player = false;
     event = no_Event;
+}
+
+
+void save_move()
+{
+    num_moves++;
+    list_of_moves = realloc(list_of_moves, num_moves * sizeof(uint8_t*));
+
+    uint8_t *move = malloc(4 * sizeof(uint8_t));
+
+    move[0] = cursor.x;
+    move[1] = cursor.y;
+    move[2] = selection.x;
+    move[3] = selection.y;
+
+    list_of_moves[num_moves - 1] = move;
 }
 
 
@@ -139,6 +171,8 @@ void handle_button_a()
     }
     else if (square_selected && move_is_legal(cursor.y, cursor.x, selection.y, selection.x, false))
     {
+        if (!replay_mode) save_move();
+
         event = move_piece_board(cursor.y, cursor.x, selection.y, selection.x, false);
 
         uint8_t piece = get_piece(selection.y, selection.x);
@@ -181,6 +215,39 @@ void handle_button_a()
 }
 
 
+void replay()
+{
+    replay_mode = true;
+    init_game();
+
+    uint16_t current_move = 0;
+
+    while (!(joypad_state & J_B))
+    { 
+        if (current_move >= num_moves)
+        {
+            replay_mode = false;
+            return;
+        }
+        if (joypad_state & J_RIGHT)
+        {
+            cursor.x = list_of_moves[current_move][0];
+            cursor.y = list_of_moves[current_move][1];
+            selection.x = list_of_moves[current_move][2];
+            selection.y = list_of_moves[current_move][3];
+
+            square_selected = true;
+            handle_button_a();
+
+            current_move++;
+            waitpadup();
+        }
+        joypad_state = joypad();
+    }
+    replay_mode = false;
+}
+
+
 void handle_button_select()
 {
     while (joypad_state & J_SELECT)
@@ -193,6 +260,11 @@ void handle_button_select()
         {
             init_game();
         }
+        else if (joypad_state & J_START)
+        {
+            replay();
+        }
+        
 
         joypad_state = joypad();
     }
